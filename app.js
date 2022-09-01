0dcf844bd457cf3a26c21c440a622e836b0ab50b
Itai Kumengisa <imehku222@gmail.com>
2022-09-01 04:01:21 -0500

    allow user to enter letters using UI keyboard

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ let numLetters = 5;
 const words = ['sauce', 'jeans', 'beans', 'horse', 'month']
 
 const game = {
-    boardState: Array(6).fill().map(() => Array(numLetters).fill('O')),
-    currentRow: 0,
-    currentCol: 0,
+    boardState: Array(6).fill().map(() => Array(numLetters).fill('')),
+    rowIndex: 0,
+    colIndex: 0,
     solution: words[Math.floor(Math.random() * words.length)]
 }
 
 drawBoard();
 
 mapBoardStateToGrid();
+
+registerInputEvents();
 
 console.log(game)
 
@@ -48,7 +50,70 @@ function addTile(board, row, col, letter=''){
 }
 
 function registerInputEvents(){
+    //Add event listeners to keyboard buttons
+
+    //get all keyboard buttons
+    const keys = document.querySelectorAll('.keyboard-btn');
+
+    //add event handlers to keys onClick event
+    keys.forEach(key => {
+        key.onclick = ({target}) => {
+            const val = target.dataset.key;
+            console.log(val)
+            
+            checkInput(val);
+        }
+    })
+
+    //handle keyboard input
     
+}
+
+function getRowWord(){
+    game.boardState[game.rowIndex].reduce((word, letter) => word + letter)
+}
+
+function checkInput(letter){
+    if(letter === 'Enter'){
+        if(game.colIndex === numLetters){
+            const word = getRowWord()
+
+            revealWord(word);
+        } else {
+            alert(`The word must be ${numLetters} letters long`)
+        }
+    }
+
+    if(letter === 'Backspace'){
+        deleteLetter();
+    }
+
+    if(letter.match(/[a-z]/i) && letter.length === 1){
+        if(game.colIndex < numLetters){
+            addLetter(letter)
+        }
+    }
+}
+
+function addLetter(letter){
+    //Add letter to game state board and ui grid
+    // console.log("Youve reached addLetter: ", letter)
+
+    //update game state
+    game.boardState[game.rowIndex][game.colIndex] = letter
+    mapBoardStateToGrid();
+    game.colIndex++;
+
+
+    console.log(game)
+}
+
+function deleteLetter(){
+
+}
+
+function revealWord(word){
+
 }
 
 function mapBoardStateToGrid(){
