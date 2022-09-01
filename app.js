ab116d8bbc0f0b82616a7a8d9f9de1f0b42dbe7e
Itai Kumengisa <imehku222@gmail.com>
2022-09-01 00:32:41 -0500

    draw the grid programmatically

---
+++ app.js
@@ -0,0 +1,32 @@
+let numLetters = 5;
+
+drawBoard();
+
+function drawBoard(){
+    for(let row = 0; row < 6; row++){
+        for(let col = 0; col < numLetters; col++){
+            //Add square to board grid
+            addTile()
+        }          
+    }
+}
+
+
+//Add square
+function addTile(){
+
+    //Select board 
+    const board = document.getElementById('board');
+
+    //Create square
+    const tile = document.createElement('div');
+
+    //add the tile class
+    tile.classList.add('tile');
+
+
+    board.appendChild(tile);
+}
+
+
+
